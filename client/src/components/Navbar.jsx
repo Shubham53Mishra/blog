105e9dd5be8a8e35c18198fcfaac13cc5c1dfc3d
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-06-27 23:18:24 +0530

    ✨ add the login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32  ' >
        <img src= {assets.logo} alt="Logo" className='w-32 sm:w-44' />
-       <button>Login
-        <img src={assets.arrow} alt=" " className='w-6 h-6' />
+       <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Login
+        <img src={assets.arrow} alt="Arrow" className='w-3' />
        </button>
     </div>
   )
