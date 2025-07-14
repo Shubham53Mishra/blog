4acd2417d204cea44cbd5b9a1fe23c982fb1f639
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-15 00:32:42 +0530

    set the hero

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { blogCategories } from '../assets/assets'
 
 const BlogList = () => {
   return (
     <div>
       <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+         {blogCategories.map((item) => (
+         <div key={item}className='relative'></div>
+         ))}
          
       </div>
-      ----blog cards will be here----
+      <div> 
+          ----blog cards will be here----
+      </div>
+   
     </div>
   )
 }
