824f35d63a594ad16484dca3c009421fb5cf7a52
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-15 00:26:10 +0530

    set the class

---
+++ client/src/components/BlogList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogList = () => {
+  return (
+    <div>
+      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+         
+      </div>
+      ----blog cards will be here----
+    </div>
+  )
+}
+
+export default BlogList
