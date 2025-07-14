0611cf29de8ad786d426b175c221a1edcbbcbbd6
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-15 01:02:18 +0530

    set folder

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { blogCategories } from "../assets/assets";
+import {motion} from "motion/react"
 
 const BlogList = () => {
     const [menu,setMenu] = useState("All")
@@ -9,7 +10,11 @@ const BlogList = () => {
         {blogCategories.map((item) => (
           <div key={item} className="relative">
             <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>{item}
-                <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>
+                {menu === item && (
+                     <motion.div layoutId='underline'
+                     className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></motion.div>
+                )}
+               
             </button>
           </div>
         ))}
