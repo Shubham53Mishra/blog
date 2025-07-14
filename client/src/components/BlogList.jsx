bffc28ef6f298abd59dbf03ab9c4f82777d8bee8
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-15 00:51:18 +0530

    set the change :

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { blogCategories } from '../assets/assets'
+import React, { useState } from "react";
+import { blogCategories } from "../assets/assets";
 
 const BlogList = () => {
+    const [menu,setMenu] = useState("All")
   return (
     <div>
-      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
-         {blogCategories.map((item) => (
-         <div key={item}className='relative'></div>
-         ))}
-         
+      <div className="flex justify-center gap-4 sm:gap-8 my-10 relative">
+        {blogCategories.map((item) => (
+          <div key={item} className="relative">
+            <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>{item}
+                <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>
+            </button>
+          </div>
+        ))}
       </div>
-      <div> 
-          ----blog cards will be here----
-      </div>
-   
+      <div>{/* ----blog cards will be here---- */}</div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogList
+export default BlogList;
