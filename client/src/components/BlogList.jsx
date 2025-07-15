5577791a8605694944d53b36da6da6beb4855cac
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-16 00:42:05 +0530

    set the p tag

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -22,7 +22,8 @@ const BlogList = () => {
           </div>
         ))}
       </div >
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">{blog_data.filter((blog)=> menu === "All" ? true : blog.category === menu).map((blog)=> <BolgCard key={blog._id} blog={blog}/>)}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">{blog_data.filter((blog)=> menu === "All" ? true : blog.category === menu).map((blog)=> <BolgCard key={blog._id} blog={blog}/>)}
+      </div>
     </div>
   );
 };
