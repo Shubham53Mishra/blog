898c2079ba2ef9e7d30f5612e4e080eef4e3b03e
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-17 00:19:16 +0530

    set the footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+      <div>
+
+      </div>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>copyright 2025 @ QuickBlog -Shubham Mishra All Right Reserved .</p>
+    </div>
+  )
+}
+
+export default Footer
