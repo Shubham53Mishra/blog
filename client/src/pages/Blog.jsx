3d662a61fd4e1595e81f033d15239bb72995cd63
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-23 00:16:12 +0530

    add the blog

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useParams} from 'react-router-dom'
 import { blog_data } from '../assets/assets'
 
 const Blog = () => {
   const {id} =useParams()
+
   const [data, setData] = useState(null) 
 
   const fetchblogData = async ()=> {
-    blog_data
-
+    const data = blog_data.find(item=> item._id === id)
+    setData(data)
    } 
 
   return (
