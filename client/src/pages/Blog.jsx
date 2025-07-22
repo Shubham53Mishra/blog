81aaa07adf88192d716b5e96b8731a233f1a2d59
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-07-23 01:01:00 +0530

    ✨ Shubham Mishra

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -24,8 +24,11 @@ const Blog = () => {
     <div className='relative'>
          <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
       <Navbar />
-      <div>
-        <p>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+      <div className='text-center mt-20 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subtitle}</h2>
+        <p className='text-gray-500'>Shubham Mishra</p>
       </div>
       <div></div>
     </div>
